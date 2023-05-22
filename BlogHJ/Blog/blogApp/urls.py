e61da01b772bcf63bc632d21d62c522b394c6186
haejun1 <a01027833227@gmail.com>
2023-05-22 16:09:39 +0900

    글 순서 바꾸기 + 작성 후 홈으로 이동

--- BlogHJ/Blog/blogApp/urls.py
+++ BlogHJ/Blog/blogApp/urls.py
@@ -1,17 +1,11 @@
 from django.urls import path
 from .views import *
 
-# 이미지를 업로드하자
-# from django.conf.urls.static import static
-# from django.conf import settings
-
 urlpatterns = [
     path('lion/', lion, name='lion'),
     path('lion/<int:pk>',lionPosting, name="lionPosting"),
     path('lion/lionNewPost/',lionNewPost),
     path('lionEdit/<int:pk>',lionEdit, name='lionEdit'),
     path('lionUpdate/<int:pk>', lionUpdate, name='lionUpdate'),
+    path('lionDel/<int:pk>', lionDel, name="lionDel"),
 ]
-
-# 이미지 URL 설정
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- BlogHJ/Blog/blogApp/views.py
+++ BlogHJ/Blog/blogApp/views.py
@@ -3,11 +3,10 @@ from .models import Blog
  #models.py에서 적었던 Blog를 import해주기
 from django.utils import timezone
 
-def lion(request): #lion이라는 함수는 서버에 요청이 들어왔을 때 lion.html을 렌더링해서 우리에게 보여줌
-    blogs = Blog.objects #Blog.objects는 admin페이지에서 확인했던 blog 안의 데이터들을 말함    #Blog는 models에 있는걸 말함
+def lion(request):
+    blogs = Blog.objects.order_by('-pub_date')
     return render(request, 'lion.html', {'blogs':blogs})
-                                        #blogs라는 변수를 template에서 쓸 떄 blogs라는 이름으로 가져오겠다
-
+                                   
 def lionPosting(request, pk):
     #pk로 개별 게시글을 검색
     blog = Blog.objects.get(pk=pk)
@@ -19,9 +18,8 @@ def lionNewPost(request):
             title=request.POST['title'],
             body=request.POST['body'],
             pub_date = timezone.now()
-            # img=request.POST['img'],
         )
-        return render(request, 'lionNewPost.html', {'newLion': newLion})
+        return redirect('/lion', {'nweLion':newLion})
     return render(request, 'lionNewPost.html')
 
 def lionEdit(request, pk): 
@@ -47,3 +45,8 @@ def lionUpdate(request, pk):
     update.pub_date = timezone.now()
     update.save()
     return redirect('/lion', pk=update.pk)
+
+def lionDel(request, pk):
+    lionDel = Blog.objects.get(pk=pk)
+    lionDel.delete()
+    return redirect('/lion')
