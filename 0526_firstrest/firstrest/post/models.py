ec2b76e0f5230e6d2440ff6a0305af1d9775c9ab
haejun1 <a01027833227@gmail.com>
2023-05-31 23:46:32 +0900

    rest실습까진 해봤는데 안되서 막 만질 예정

--- 0526_firstrest/firstrest/post/models.py
+++ 0526_firstrest/firstrest/post/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 100)
+    body = models.TextField()
+
+#Username : haejun
+#Email addresss : a01027833227@gmail.com
+#password : 0000
