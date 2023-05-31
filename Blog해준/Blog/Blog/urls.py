26305cda5f4d2169f80fcb4f56b9d33a74d1cc61
haejun1 <a01027833227@gmail.com>
2023-05-31 22:00:53 +0900

    Blog해준 삭제

--- Blog해준/Blog/Blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-import blogApp.views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blogApp.urls')), #view와 url을 연결시켜준다, main에서의 urls을 불러온다
-]
--- Blog해준/Blog/blogApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Blog
-from django.shortcuts import render
-
-# Register your models here.
-admin.site.register(Blog) #admin.py에 model을 등록하는 명령어
-                        #admin이 게시글에 접근 가능하게 해 줌
