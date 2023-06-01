7518ecedca9a874e5370ec6f5ecd8fa4df82a1b6
haejun1 <a01027833227@gmail.com>
2023-06-01 15:50:43 +0900

    rest api 잘 된다

--- 0526_firstrest/firstrest/firstrest/urls.py
+++ 0526_firstrest/firstrest/firstrest/urls.py
@@ -1,12 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-import post.views
-from rest_framework.routers import DefaultRouter
-
-router = DefaultRouter()
-router.register('post', post.views.PostViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('', include('post.urls')),
 ]
--- 0526_firstrest/firstrest/post/views.py
+++ 0526_firstrest/firstrest/post/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from .models import Post
-from .serializer import PostSerializer
+from .serializers import PostSerializer
 from rest_framework import viewsets
 
 # Create your views here.
-class PostViewSet(viewsets.ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet): 
+    #ModelViewSet은 CRUD를 지원
     queryset = Post.objects.all()
     serializer_class = PostSerializer
